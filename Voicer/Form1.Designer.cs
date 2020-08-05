1626321dbf4967af542c104ad8e99b64fa2fc7e4
Raphipod <podraphi@googlemail.com>
2020-08-05 14:48:38 +0200

    Projektdateien hinzufügen.

---
+++ Voicer/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Voicer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.voiceBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 157);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Available voices:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 202);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Speak...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // voiceBox
+            // 
+            this.voiceBox.FormattingEnabled = true;
+            this.voiceBox.Location = new System.Drawing.Point(16, 33);
+            this.voiceBox.Margin = new System.Windows.Forms.Padding(2);
+            this.voiceBox.Name = "voiceBox";
+            this.voiceBox.Size = new System.Drawing.Size(320, 82);
+            this.voiceBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 254);
+            this.Controls.Add(this.voiceBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Voicer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox voiceBox;
+    }
+}
+
---
+++ Voicer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Voicer
+{
+    public partial class Form1 : Form
+    {
+        public static SpeechSynthesizer ss = new SpeechSynthesizer();
+        public Dictionary<string, string> voiceDict = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            SpeechSynthesizer ss = new SpeechSynthesizer();
+
+            foreach (InstalledVoice voice in ss.GetInstalledVoices())
+            {
+                voiceDict.Add(voice.VoiceInfo.Name, voice.VoiceInfo.Description);
+            }
+            
+
+            foreach (var voiceInfoPair in voiceDict)
+            {
+                voiceBox.Items.Add(voiceInfoPair.Value);
+            }
+
+            voiceBox.SelectedIndex = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedVoice = voiceBox.SelectedItem.ToString();
+            string voiceName = voiceDict.FirstOrDefault(x => x.Value == selectedVoice).Key;
+            ss.SelectVoice(voiceName);
+            ss.SpeakAsync(textBox1.Text.ToString());
+        }
+    }
+}
